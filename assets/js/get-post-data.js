1f0d7f4c2796526599d37e6720d4262a8b97d8dd
Manyiri-Developers <mrmwarobaya@gmail.com>
2025-06-23 11:03:33 -0600

    Update get-post-data.js

--- assets/js/get-post-data.js
+++ assets/js/get-post-data.js
@@ -51,7 +51,10 @@ document.addEventListener('DOMContentLoaded', () => {
       method: 'POST',
       body: JSON.stringify(data),
       headers: {
-        'Content-Type': 'application/json'
+        // 'Content-Type': 'application/json'
+         'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'POST',
+        'Access-Control-Allow-Headers': 'Content-Type',
       }
     })
       .then(response => {
