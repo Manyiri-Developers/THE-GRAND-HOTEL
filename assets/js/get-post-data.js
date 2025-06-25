8acd7e79ba4ad734476d26df526a3c63268a258f
https://github.com/Manyiri-Developers/THE-GRAND-HOTEL.git <mrmwarobaya@gmail.com>
2025-06-24 22:52:00 -0600

    USING NEW  VERCEL PROXY

--- assets/js/get-post-data.js
+++ assets/js/get-post-data.js
@@ -46,15 +46,13 @@ document.addEventListener('DOMContentLoaded', () => {
     loading.style.display = 'block';
 
     // Send data to Google Apps Script
-    fetch('https://script.google.com/macros/s/AKfycbxKEkaOqbCVsbR-yBOECfpEiNWB8YbcAxm2aVdgHehMZq-1-SNCsrqwvunt592NYnqe/exec', { // Use /exec for production
+    // Use http://localhost:3001/api/book for local testing
+
+    fetch('https://grandhotel-proxy.vercel.app/api/book ', { // Use /vercel link for production
       method: 'POST',
       body: JSON.stringify(data),
       headers: {
          'Content-Type': 'application/json'
-        // 'Access-Control-Allow-Origin': '*',
-        // 'Access-Control-Allow-Methods': 'POST',
-        // 'Access-Control-Allow-Headers': 'Content-Type',
-        
       }
     })
       .then(response => {
